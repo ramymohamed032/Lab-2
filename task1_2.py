e6010325caa5ae0d945530a3d49774c17e64f699
Ramy Mohamed <ramymhmd198@gmail.com>
2024-11-04 02:21:17 +0300

    task1_2.py

---
+++ task1_2.py
@@ -0,0 +1,9 @@
+'''Module providing a function reading csv file'''
+
+from csv import reader
+
+# Color codes for output
+RED = '\u001b[41m'
+LIGHT_GREEN = '\u001b[42m'
+YELLOW = '\u001b[43m'
+END = '\u001b[0m'
