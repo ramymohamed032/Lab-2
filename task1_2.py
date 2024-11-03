85ddc9b50e7a634d46bf1e54accc9ebf1cbdc93b
Ramy Mohamed <ramymhmd198@gmail.com>
2024-11-04 02:23:16 +0300

    Update task1_2.py

--- task1_2.py
+++ task1_2.py
@@ -7,3 +7,26 @@ RED = '\u001b[41m'
 LIGHT_GREEN = '\u001b[42m'
 YELLOW = '\u001b[43m'
 END = '\u001b[0m'
+
+# Поиск по автору
+
+while True:
+    count = 0
+    request = input("Enter a request: ")
+    if request.lower() == 'stop':
+        print("Thank you for using!")
+        break
+    else:
+        with open('books-en.csv', 'r', encoding='windows-1251') as csvfile:
+            file = reader(csvfile, delimiter=';')
+            next(file)  
+            for row in file:
+                lower_case = row[2].lower()  
+                index = lower_case.find(request.lower())
+                if index != -1:
+                    if 1997 <= int(row[3]) <= 2000:  
+                        count += 1
+                        print(
+                            f"{row[1]} "
+                            f"(Author: {row[2]})"
+                        )
