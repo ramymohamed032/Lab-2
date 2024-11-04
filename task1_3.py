9af8aa6573d00eaf7385bc7e50c193bd2568bb99
Ramy Mohamed <ramymhmd198@gmail.com>
2024-11-04 04:25:49 +0300

    task1_3.py

--- task1_3.py
+++ task1_3.py
@@ -5,20 +5,20 @@
 from csv import reader
 from random import randint
 
-# Генератор библиографических ссылок
+# Генератор библиографических ссылок 
 
-output = open('Book_result.txt', 'w', encoding='windows-1251')
+# Open the output file
+with open('Book_result.txt', 'w', encoding='windows-1251') as output:
+    # Open and read the CSV file
+    with open('books-en.csv', 'r', encoding='windows-1251') as csvfile:
+        file = reader(csvfile, delimiter=";")
+        books_data = [row for row in file]
+        books_data.pop(0)  # Remove the header row
 
-with open('books-en.csv', 'r', encoding='windows-1251') as csvfile:
-    file = reader(csvfile, delimiter=";")
-    books_data = [i for i in file]
-    books_data.pop(0)  # Remove the header row
-
-    for row in range(1, 21):  # Generate 20 bibliographic references
-        random_book = books_data[randint(1, len(books_data) - 1)]
-        # Format: <Author>. <Title> - <Year>
-        print(f'{row}. {random_book[2]}. {random_book[1]} 
-        - {random_book[3]}')
-        output.write(f'{row}. {random_book[2]}. {random_book[1]} - {random_book[3]}\n')
-
-output.close()
+        # Generate 20 random bibliographic references
+        for row_num in range(1, 21):
+            random_book = books_data[randint(1, len(books_data) - 1)]
+            # Format: <Author>. <Title> - <Year>
+            reference = f"{row_num}. {random_book[2]}. {random_book[1]} - {random_book[3]}"
+            print(reference)
+            output.write(reference + "\n")
