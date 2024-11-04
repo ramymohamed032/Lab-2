265bd1a0391cd0d852df940ee7df65c56ee19832
Ramy Mohamed <ramymhmd198@gmail.com>
2024-11-04 03:09:40 +0300

    task1_4_extra.py

--- task1_4_extra.py
+++ task1_4_extra.py
@@ -9,3 +9,12 @@ with open('books-en.csv', 'r', encoding='windows-1251') as csvfile:
     file = reader(csvfile, delimiter=';')
     books_data = [i for i in file]
     books_data.pop(0)  # Remove the header row
+
+
+    # Collect publishers from the specified column
+    for row in books_data:
+        publishers.append(row[4])  # Assuming publisher is in column index 4
+
+# Display each unique publisher
+for publisher in set(publishers):
+    print(publisher)
